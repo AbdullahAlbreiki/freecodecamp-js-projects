67d61a82c279bbb28beb2bf07f2e11c6b8386f2b
Abdullah Albreiki <abdallahalbraiky@gmail.com>
2025-09-05 03:26:23 +0400

    New lab exercise with REACT

---
+++ react-lab-reusable-footer/index.jsx
@@ -0,0 +1,27 @@
+export const Footer = () => {
+  return(
+    <footer>
+      <div className="list-div">
+        <ul>
+          <li className="list-item"><a href="#">Fitness Dashboard</a></li>
+          <li className="list-item"><a href="#">Services</a></li>
+        </ul>
+        <ul>
+          <li className="list-item"><a href="#">Watch Videos</a></li>
+          <li className="list-item"><a href="#">Discord</a></li>
+        </ul>
+        <ul>
+          <li className="list-item"><a href="#">Privacy Policy</a></li>
+          <li className="list-item"><a href="#">Terms & Conditions</a></li>
+        </ul>
+      </div>
+      <p className="copyright">© 2024 Fitness Dashboard. All Rights Reserved.</p>
+      <div className="link-div">
+        <a href="#">🎮</a>
+        <a href="#">🐦</a>
+        <a href="#">💻</a>
+        <a href="#">🏀</a>
+      </div>
+    </footer>
+  )
+};
